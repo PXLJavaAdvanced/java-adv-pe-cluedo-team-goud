a59aea82ff002865158fbf1407f65ae4dd90bd94
StefBrackez <11800716@student.pxl.be>
2021-12-14 19:58:52 +0100

    Envelope base created.

---
+++ src/main/java/be/pxl/ja/Envelope.java
@@ -0,0 +1,13 @@
+package be.pxl.ja;
+
+public class Envelope {
+    private List<T>  secrets;
+
+    private void addSecret(){}
+
+    private T removeSecret(){}
+
+    private boolean isSecret(){
+        return false;
+    }
+}
