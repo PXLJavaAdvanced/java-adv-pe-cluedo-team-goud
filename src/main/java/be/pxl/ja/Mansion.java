0ae538d1665c261ad9b9051392386667343778eb
nielsvanderstappen <43780194+nielsvanderstappen@users.noreply.github.com>
2021-12-14 19:51:07 +0100

    Mansion class has been created and completed. + Also CluedoException

---
+++ src/main/java/be/pxl/ja/Mansion.java
@@ -0,0 +1,34 @@
+package be.pxl.ja;
+
+import java.util.List;
+import java.util.Optional;
+
+public class Mansion extends Assembly<Room> {
+
+    private static final String HALL = "hall";
+
+    public Mansion(List<Room> assemblyParts) {
+        super(assemblyParts);
+    }
+
+    public Room getHall() {
+        return getPart(HALL)
+                .orElseThrow(() -> new CluedoException("Every mansion must have a hall"));
+    }
+
+    public int getNumberOfRooms() {
+        return getNumberOfParts();
+    }
+
+    public Optional<Room> getRoom(String room) {
+        return getPart(room);
+    }
+
+    public Room getRoom(int room) {
+        return getPart(room);
+    }
+
+    public List<Room> getRooms() {
+        return getParts();
+    }
+}
